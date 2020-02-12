4cf035002bdc03646b7aca7d5b08afdfcee886fb
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-12 23:39:54 +0800

    [icourse163] Add property lookup for Course class

--- course.py
+++ course.py
@@ -1,10 +1,53 @@
 from util import camel_to_snake
 
-# s13.achievementStatus=1;s13.applyConvertChannelStatus=null;s13.applyMoocStatus=0;s13.applyPassedTermId=null;s13.asynPrice=null;s13.bigPhotoUrl="http://edu-image.nosdn.127.net/E2A2D9422AE2D9E37C24A59A94984CB8.png?imageView&thumbnail=510y288&quality=100";s13.certApplyEndTime=null;s13.certApplyStartTime=null;s13.certNo=null;s13.certStatus=20;s13.chargeCertStatus=20;s13.chargeableCert=1;s13.closeVisableStatus=0;s13.copied=1;s13.copyRight=null;s13.copyTime=null;s13.courseId=62001;s13.courseName=null;s13.duration="";s13.endTime=1561563000000;s13.enrollCount=1763;s13.fromTermId=1003251017;s13.fromTermMode=0;s13.hasEnroll=true;s13.id=1206088220;s13.lectorPanels=s34;s13.lessonsCount=72;s13.mode=0;s13.orderPrice=null;s13.ordinaryEditors=null;s13.originMocTermCopyRight=null;s13.originalCourseChannel=null;s13.originalPrice=0.00;s13.price=0.00;s13.publishStatus=2;s13.schoolId=8008;s13.schoolPanel=null;s13.scoreCardDto=null;s13.selfMocTermCopyright=null;s13.specialChargeableTerm=false;s13.spocToOocStatus=0;s13.startTime=1553479200000;s13.syncPrice=null;
-# s13.jsonContent="spContent=";
-
 
 class Course(object):
+    """
+    'achievement_status': '1',
+    'course_id': '62001',
+    'end_time': '1561563000000',
+    'enroll_count': '1763',
+    'from_term_id': '1003251017',
+    'id': '1206088220',
+    'json_content': '"spContent=本课程面向无编程基础，并致力于编程的学生，使其掌握程序设计的基本概念、思想和方法，并培养其计算思维能力。通过本课程学习，学生能够掌握程序设计语言的基本语法、结构化程序设计思想和方法，养成良好的编程风格；最终能在这些方法和思想的指导下，不受编程语言限制，随心写出解决实际问题的程序。"',
+    'lessons_count': '72',
+    'publish_status': '2',
+    'school_id': '8008',
+    'start_time': '1553479200000',
+    'apply_convert_channel_status': 'null',
+    'apply_mooc_status': '0',
+    'apply_passed_term_id': 'null',
+    'asyn_price': 'null',
+    'big_photo_url': '"http://edu-image.nosdn.127.net/E2A2D9422AE2D9E37C24A59A94984CB8.png?imageView&thumbnail=510y288&quality=100"',
+    'cert_apply_end_time': 'null',
+    'cert_apply_start_time': 'null',
+    'cert_no': 'null',
+    'cert_status': '20',
+    'charge_cert_status': '20',
+    'chargeable_cert': '1',
+    'close_visable_status': '0',
+    'copied': '1',
+    'copy_right': 'null',
+    'copy_time': 'null',
+    'course_name': 'null',
+    'duration': '""',
+    'from_term_mode': '0',
+    'has_enroll': 'true',
+    'lector_panels': 's34',
+    'mode': '0',
+    'order_price': 'null',
+    'ordinary_editors': 'null',
+    'origin_moc_term_copy_right': 'null',
+    'original_course_channel': 'null',
+    'original_price': '0.00',
+    'price': '0.00',
+    'school_panel': 'null',
+    'score_card_dto': 'null',
+    'self_moc_term_copyright': 'null',
+    'special_chargeable_term': 'false',
+    'spoc_to_ooc_status': '0',
+    'sync_price': 'null'
+    """
     def __init__(self, *initial_data, **kwargs):
         for dictionary in initial_data:
             for key in dictionary:
