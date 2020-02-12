4c6c745bfb7bddd67a73611228b91fd8c31f598a
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-12 22:50:08 +0800

    [icourse163] Finish get_all_students_score()

--- icourse163.py
+++ icourse163.py
@@ -80,6 +80,63 @@ def get_term_statistic():
     return test_list
 
 
+def get_all_students_score():
+    # http://www.icourse163.org/collegeAdmin/termManage/1206772205.htm#/tp/manageStudent
+    request_student_score_url = "http://www.icourse163.org/mm-tiku/web/j/mocTermScoreSummaryRpcBean.getStudentScorePagination.rpc?csrfKey={}".format(http_session_id)
+
+    """
+    sortCriterial: "total_score_plus_bonus desc"
+    DEFAULT_PAGE_SIZE: 10
+    DEFAULT_PAGE_INDEX: 1
+    DEFAULT_TOTLE_PAGE_COUNT: 1
+    DEFAULT_TOTLE_COUNT: 0
+    DEFAULT_OFFSET: 0
+    pageSize: 20
+    pageIndex: 1
+    totlePageCount: 61
+    totleCount: 1203
+    offset: 0
+    limit: 20
+    """
+    form_data = {
+        "termId": term_id,
+        "pIndex": 1,
+        "pSize": 20,
+        "rangeType": 1,
+        "groupId": -1,
+        "sort": 12,
+        "searchName": "",
+    }
+
+    response = session.post(url=request_student_score_url, data=form_data).text
+    response = json.loads(response.content)
+    summary_list = []
+
+    for summary in response["result"]["list"]:
+        term_score_summary = TermScoreSummary()
+        term_score_summary.summary_id = summary["summaryId"]
+        term_score_summary.term_id = summary["termId"]
+        term_score_summary.member_id = summary["memberId"]
+        term_score_summary.test_score = summary["testScore"]
+        term_score_summary.assginment_score = summary["assignmentScore"]
+        term_score_summary.exam_score = summary["examScore"]
+        term_score_summary.discuss_score = summary["discussScore"]
+        term_score_summary.reply_count = summary["replyCount"]
+        term_score_summary.vote_count = summary["voteCount"]
+        term_score_summary.outside_score = summary["outsideScore"]
+        term_score_summary.total_score = summary["totalScore"]
+        term_score_summary.bonus_score = summary["bonusScore"]
+        term_score_summary.total_score_with_bonus = summary["totalScoreWithBonus"]
+        term_score_summary.nick_name = summary["nickName"]
+        term_score_summary.real_name = summary["realName"]
+        term_score_summary.level = summary["level"]
+        term_score_summary.final_score = summary["finalScore"]
+
+        summary_list.append(term_score_summary)
+
+    return summary_list
+
+
 if __name__ == "__main__":
     session = get_login_session()
     http_session_id = session.cookies["NTESSTUDYSI"]
