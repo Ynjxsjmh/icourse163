53e85499ed393fe77289697d21c093113f6d187d
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-14 10:05:57 +0800

    [icourse163] Add find the most common element in list

--- utils/util.py
+++ utils/util.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import inflection
+from statistics import mode, StatisticsError
 
 
 # https://stackoverflow.com/questions/16058065
@@ -10,3 +11,15 @@ def raw_unicode_escape(string: str) -> str:
 
 def camel_to_snake(name: str) -> str:
     return inflection.underscore(name)
+
+
+def most_common(l):
+    try:
+        return mode(l)
+    except StatisticsError as e:
+        # will only return the first element if no unique mode found
+        if 'no unique mode' in e.args[0]:
+            return l[0]
+        # this is for "StatisticsError: no mode for empty data"
+        # after calling mode([])
+        raise
