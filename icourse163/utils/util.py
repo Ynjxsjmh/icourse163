df33a8a900112145a12b2af5e8f412640ce20087
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-14 22:24:53 +0800

    [icourse163] Add get_key_value method

--- icourse163/utils/util.py
+++ icourse163/utils/util.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import json
 import inflection
 import traceback
@@ -39,3 +40,26 @@ def most_common(l):
         # this is for "StatisticsError: no mode for empty data"
         # after calling mode([])
         raise
+
+
+def get_key_value(line):
+    obj_regex = re.compile(r'(s\d+\.)')
+
+    result_list = re.findall(obj_regex, line)
+
+    if len(result_list) == 0:
+        return dict()
+
+    obj = most_common(result_list)
+    results = line.split(obj)
+
+    result_list = []
+
+    for result in results:
+        if result == "":
+            pass
+        else:
+            # 去除最后的分号，然后按第一个等号分割成 key value 对
+            result_list.append(result[:-1].split("=", 1))
+
+    return dict(result_list)
