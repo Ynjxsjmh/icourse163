5d30944809a441004fb665be5e8d28b56e6a90f5
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-02-14 19:17:12 +0800

    [icourse163] Transfer Unicode to Chinese characters

--- icourse163/utils/util.py
+++ icourse163/utils/util.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 import json
 import inflection
+import traceback
 from statistics import mode, StatisticsError
 
 
 # https://stackoverflow.com/questions/16058065
 def raw_unicode_escape(string: str) -> str:
-    return json.loads('{}'.format(string))
+    """
+    'name': '"\\u9AD8\\u7EA7\\u8BED"',
+    'imgUrl': '"http://edu-im"'
+    1. 将 name 转化为中文
+    2. 将两个字符串一起的转成一个字符串
+    """
+    result = ""
+    try:
+        result = json.loads('{}'.format(string))
+    except AttributeError:
+        try:
+            result = json.loads('"{}"'.format(string))
+        except AttributeError:
+            traceback.print_exc()
+    return result
 
 
 def camel_to_snake(name: str) -> str:
